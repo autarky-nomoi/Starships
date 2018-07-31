acde760b20ed8a65f883a950c63bb2010f795cf7
Jordan Marcus <jormarcus92@gmail.com>
2018-07-31 17:17:35 -0400

    updated secrets and commented out some tests

--- server/api/wishlist.js
+++ server/api/wishlist.js
@@ -1,60 +1,60 @@
-const router = require('express').Router();
-const {Wishlist,Ship}  = require('../db/models');
+// const router = require('express').Router();
+// const {Wishlist,Ship}  = require('../db/models');
 
-module.exports = router;
+// module.exports = router;
 
-//get all ships by user's wishlist
-router.get('/:id', async (req, res, next) => {
-  try {
-    const result = await Wishlist.findAll({
-      include : [{model : Ship}],
-      userId : req.params.userId
-    })
-    res.json(result)
+// //get all ships by user's wishlist
+// router.get('/:id', async (req, res, next) => {
+//   try {
+//     const result = await Wishlist.findAll({
+//       include : [{model : Ship}],
+//       userId : req.params.userId
+//     })
+//     res.json(result)
 
-  } catch (error) {
-    console.log(error)
-  }
-});
-
-
-
-router.post('/', async (req,res,next)=>{
-  try {
-    const found = await Wishlist.findOne({
-      where : {
-        userId : req.body.userId,
-        starshipId : req.body.starshipId
-      }
-    })
-    if(found){
-      res.json(found)
-    }else{
-      const addedToWish = await Wishlist.create({
-        userId : req.body.userId,
-        starshipId : req.body.starshipId
-      })
-      res.json(addedToWish)
-    }
-  } catch (error) {
-    next(error)
-  }
-
-})
+//   } catch (error) {
+//     console.log(error)
+//   }
+// });
 
 
 
-router.delete('/',async (req,res,next) => {
-  try {
-    await Wishlist.destroy({
-      where : {
-        userId : req.body.userId,
-        starshipId : req.body.starshipId
-      }
-    })
-    res.json(202)
-  } catch (error) {
-    next(error)
-  }
+// router.post('/', async (req,res,next)=>{
+//   try {
+//     const found = await Wishlist.findOne({
+//       where : {
+//         userId : req.body.userId,
+//         starshipId : req.body.starshipId
+//       }
+//     })
+//     if(found){
+//       res.json(found)
+//     }else{
+//       const addedToWish = await Wishlist.create({
+//         userId : req.body.userId,
+//         starshipId : req.body.starshipId
+//       })
+//       res.json(addedToWish)
+//     }
+//   } catch (error) {
+//     next(error)
+//   }
 
-})
+// })
+
+
+
+// router.delete('/',async (req,res,next) => {
+//   try {
+//     await Wishlist.destroy({
+//       where : {
+//         userId : req.body.userId,
+//         starshipId : req.body.starshipId
+//       }
+//     })
+//     res.json(202)
+//   } catch (error) {
+//     next(error)
+//   }
+
+// })
