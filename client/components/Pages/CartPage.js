9e423222c4716cb911c33a7c7fecd7a874600bf4
Jordan Marcus <jormarcus92@gmail.com>
2018-07-31 15:19:53 -0400

    commiting so i can pull

--- client/components/Pages/CartPage.js
+++ client/components/Pages/CartPage.js
@@ -31,80 +31,48 @@ class CartPage extends Component {
     const Usercart = this.props.cart
 
     console.log(this.props.cart)
-    return (
-      <div className='cart'>
-      <div className='products'>
-            <h1 className='color center' > Your Cart </h1>
 
-            <div className='list-item-cal'>
-            <hr />
-
-            <div className='list-item'>
-            <p className='color'>Item</p>
-            </div>
-
-            <div className='list-price'>
-            <p className='color'>Price</p>
-            </div>
-
-            <div className='list-quantity '>
-            <p className='color'>Quantity</p>
-            </div>
-            <hr />
-
-            <div className='ship-list '>
-            {console.log((Object.keys(user).length === 0))}
-            { !user.id ?<h1>guset cart </h1> :(Usercart.map((item,index)=>{
-              return (
-                <CartItem userId={this.props.user.id} key={index} ship={item}/>
-              )
-            })) 
-              }
+    if (user) {
+      
+      return (
+        <div className='cart'>
+        <div className='products'>
+              <h1 className='color center' > Your Cart </h1>
+  
+              <div className='list-item-cal'>
+              <hr />
+  
+              <div className='list-item'>
+              <p className='color'>Item</p>
+              </div>
+  
+              <div className='list-price'>
+              <p className='color'>Price</p>
+              </div>
+  
+              <div className='list-quantity '>
+              <p className='color'>Quantity</p>
+              </div>
+              <hr />
+  
+              <div className='ship-list '>
+              { Usercart.map((item, index) => {
+                return (
+                  <CartItem userId={this.props.user.id} key={index} ship={item}/>
+                )
+              }) 
+                }             
               
-            
-            </div>
-
-
-
-      </div>
-        
-      </div>
-            <Summary subtotal={subtotal} shipCount={shipCount} Usercart={Usercart}/>
-      {/* <div className='total'>
-        <div className='summary'>
-        <h3> Summary ({shipCount} Ships) </h3>
-
-
-        <div className='container'>
-        <p className='inline-block'> Subtotal </p>
-        <p className='inline-block right'>${subtotal}</p>
+              </div>
+  
         </div>
-
-        <div className='container'>
-        <p className='inline-block'> Shipping </p>
-        <p className='inline-block right'>$0</p>
+          
         </div>
-
-
-        <div className='container'>
-        <p className='inline-block'> Est. Taxes </p>
-        <p className='inline-block right'>$0</p>
+              <Summary subtotal={subtotal} shipCount={shipCount} Usercart={Usercart}/>
+          
         </div>
-
-        <hr />
-        <div className='container'>
-        <p className='inline-block'><b>Total</b></p>
-        <p className='inline-block right'>${subtotal}</p>
-        </div>
-
-        <div className='checkout'>
-        <button className="button button2">Checkout</button>
-        </div>
-        </div>
-      </div> */}
-        
-      </div>
-    )
+      )
+    }
   }
 }
 
